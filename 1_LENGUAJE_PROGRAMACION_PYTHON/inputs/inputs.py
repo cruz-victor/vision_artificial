5ff047c473b3d70077bfd6d1066f0ccb9f0b314e
Victor CG <ubicuosoft@gmail.com>
2024-07-05 11:20:45 -0400

    inputs

---
+++ 1_LENGUAJE_PROGRAMACION_PYTHON/inputs/inputs.py
@@ -0,0 +1,8 @@
+#Pedir un dato al usuario
+name=input("Dame tu nombre: ")
+print(f'El nombre es: {name}')
+
+#Pedir un numero al usuario
+number=input("dame un numero: ")
+result=int(number)*2
+print(result)

1f870b5bf7bc197bf2c079cbc34e38eee5cc6a05
Victor CG <ubicuosoft@gmail.com>
2024-07-10 17:30:31 -0400

    built-in function

---
+++ 1_LENGUAJE_PROGRAMACION_PYTHON/funciones/build-in.py
@@ -0,0 +1,23 @@
+#Encontrando el numero mayor de una lista
+numbers=[4,7,1,42,15]
+larger_number=max(numbers)
+print(larger_number)
+smaller_number=min(numbers)
+print(smaller_number)
+
+#Redondeando a 6 decimales
+number=round(12.345678*100)/100
+print(number)
+
+number_two=round(12.345678,3)
+print(number_two)
+
+result_bool=bool(1)
+print(result_bool)
+
+result_all=all([231,False,[1,2,3]])
+print(result_all)
+
+total_sum=sum(numbers)
+print(total_sum)
+

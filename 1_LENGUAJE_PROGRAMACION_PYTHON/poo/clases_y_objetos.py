5a0f4e9fe868a9a05d491013027760620f3cf92c
Victor CG <ubicuosoft@gmail.com>
2024-07-18 12:48:49 -0400

    Clases con atributos estaticos

--- 1_LENGUAJE_PROGRAMACION_PYTHON/poo/clases_y_objetos.py
+++ 1_LENGUAJE_PROGRAMACION_PYTHON/poo/clases_y_objetos.py
@@ -2,4 +2,15 @@
 #Es un tipo de objeto
 #Permite definir las caractiristicas y comportamientos
 
-class 
+class Celular():
+    marca="Samsung"
+    modelo="S23"
+    camara="15Mp"
+    
+celular_1=Celular()
+celular_2=Celular()
+celular_3=Celular()
+
+print(celular_1.marca)
+print(celular_1.modelo)
+print(celular_1.camara)

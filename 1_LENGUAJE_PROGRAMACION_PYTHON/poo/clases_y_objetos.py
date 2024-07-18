444c9ae75c551f23d5e2c32dd45b3bba2a416dd3
Victor CG <ubicuosoft@gmail.com>
2024-07-18 12:46:34 -0400

    Programacion secuencial

---
+++ 1_LENGUAJE_PROGRAMACION_PYTHON/poo/clases_y_objetos.py
@@ -0,0 +1,5 @@
+#Clase
+#Es un tipo de objeto
+#Permite definir las caractiristicas y comportamientos
+
+class 
---
+++ 1_LENGUAJE_PROGRAMACION_PYTHON/poo/sin_poo.py
@@ -0,0 +1,17 @@
+# Programacion secuencial 1
+celular_1_marca="Samsung"
+celular_1_modelo="S23"
+celular_1_camara="15Mp"
+
+celular_2_marca="Apple"
+celular_2_modelo="A15"
+celular_2_camara="14Mp"
+
+celular_3_marca="Xiamoi"
+celular_3_modelo="K10"
+celular_3_camara="10Mp"
+
+# Programacion secuencial 2
+celular_1=["Samsung","S23","15Mp"]
+celular_2=["Apple","A15","14Mp"]
+celular_3=["Xioami","K10","10Mp"]
